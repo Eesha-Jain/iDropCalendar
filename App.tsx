3a7094c37ab1051b78c04184860919d6f5bf6a11
Eesha-Jain <eesha.jain801@gmail.com>
2021-08-03 11:46:38 -0700

    Integrated Open Sans font

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  
   if (!isLoadingComplete) {
     return null;
   } else {
--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -15,7 +15,16 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'os-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+          'os-bolditalic': require('../assets/fonts/OpenSans-BoldItalic.ttf'),
+          'os-extrabold': require('../assets/fonts/OpenSans-ExtraBold.ttf'),
+          'os-extrabolditalic': require('../assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
+          'os-italic': require('../assets/fonts/OpenSans-Italic.ttf'),
+          'os-light': require('../assets/fonts/OpenSans-Light.ttf'),
+          'os-lightitalic': require('../assets/fonts/OpenSans-LightItalic.ttf'),
+          'os-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+          'os-semibold': require('../assets/fonts/OpenSans-SemiBold.ttf'),
+          'os-semibolditalic': require('../assets/fonts/OpenSans-SemiBoldItalic.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
